61c57b607fdfaaa3417d26ac5d273329cd8c027b
hgmtrebing <hgmtrebing@outlook.com>
2017-12-28 13:18:00 -0500

    Beginning of the creation of GUI in JavaFX. Creation of GUI.fxml and GuiController.java.

---
+++ src/main/java/us/hgmtrebing/calculator/GuiController.java
@@ -0,0 +1,23 @@
+package us.hgmtrebing.calculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+//TODO - figure out how to make controller class
+public class GuiController extends Application{
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        FXMLLoader loader = new FXMLLoader();
+    }
+}
